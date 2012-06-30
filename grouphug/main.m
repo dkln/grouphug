45fc4719dfd872a0f36f1be7e1f6493ecd6adde6
Diederick Lawson <diederick@altovista.nl>
2012-06-30 21:20:08 +0200

    Initial Commit

---
+++ grouphug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  grouphug
+//
+//  Created by Diederick Lawson on 6/30/12.
+//  Copyright (c) 2012 Finalist IT Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
